bbdc7991c83d49ebf349ca507f4d1c1e016694d9
wsj <wsj@cdtrust.com>
2025-01-02 23:16:36 +0800

    文字样式修改

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main( ){
+  return runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
 
   @override
-  Widget build(BuildContext buildContext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("hello word"),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Text(
+          '这是一行文本',
+          textAlign: TextAlign.left,
+          overflow: TextOverflow.ellipsis,
+          strutStyle: StrutStyle(leading: 2, fontSize: 30),
+          softWrap: false,
+          style: TextStyle(
+            background: Paint()..color = Colors.green,
+            color: Colors.black,
+            decoration: TextDecoration.combine([TextDecoration.underline, TextDecoration.overline]),
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold
+          ),
+        )
+      ),
     );
   }
+
 }
